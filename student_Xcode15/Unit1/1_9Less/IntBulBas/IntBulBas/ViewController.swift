af41e798ada1e6a095bb54efa1c42e5b98f2cb77
icazajimenezc7853 <142323737+icazajimenezc7853@users.noreply.github.com>
2024-11-05 11:10:19 -0500

    1_9Lesson completed

--- student_Xcode15/Unit1/1_9Less/IntBulBas/IntBulBas/ViewController.swift
+++ student_Xcode15/Unit1/1_9Less/IntBulBas/IntBulBas/ViewController.swift
@@ -8,15 +8,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
     
+    @IBOutlet var mainLabel: UILabel!
     
-    
-    
+    @IBOutlet var changeTitle: UIButton!
+     
+    @IBAction func pressButton(_ sender: Any) {
+        mainLabel.text = "This app rocks!"
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        changeTitle.tintColor = .red
     }
 
 
