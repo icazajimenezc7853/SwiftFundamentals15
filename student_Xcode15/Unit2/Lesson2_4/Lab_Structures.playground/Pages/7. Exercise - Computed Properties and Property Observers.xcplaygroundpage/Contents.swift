84bdc6a488fca29360e55476725ce791adce1e63
icazajimenezc7853 <142323737+icazajimenezc7853@users.noreply.github.com>
2024-11-15 19:48:43 -0500

    Lab structures update

--- student_Xcode15/Unit2/Lesson2_4/Lab_Structures.playground/Pages/7. Exercise - Computed Properties and Property Observers.xcplaygroundpage/Contents.swift
+++ student_Xcode15/Unit2/Lesson2_4/Lab_Structures.playground/Pages/7. Exercise - Computed Properties and Property Observers.xcplaygroundpage/Contents.swift
@@ -7,32 +7,53 @@ struct Rectangle {
     var width: Int
     var height: Int
     
+    // Computed property to calculate the area
+    var area: Int {
+        return width * height
+    }
 }
 
-
+let myRectangle = Rectangle(width: 10, height: 5)
+print("Area of Rectangle: \(myRectangle.area)")
 /*:
  In the `Height` struct below, height is represented in both inches and centimeters. However, if `heightInInches` is changed, `heightInCentimeters` should also adjust to match it. Add a `didSet` to each property that will check if the other property is what it should be, and if not, sets the proper value. If you set the value of the other property even though it already has the right value, you will end up with an infinite loop of each property setting the other.
  
  Create an instance of `Height` and then change one of its properties. Print out the other property to ensure that it was adjusted accordingly.
  */
 struct Height {
-    var heightInInches: Double
-    
-    var heightInCentimeters: Double
-    
-    init(heightInInches: Double) {
-        self.heightInInches = heightInInches
-        self.heightInCentimeters = heightInInches*2.54
+    var heightInInches: Double {
+        didSet {
+            // Update heightInCentimeters when heightInInches changes
+            heightInCentimeters = heightInInches * 2.54
+        }
     }
     
+    var heightInCentimeters: Double {
+        didSet {
+            // Update heightInInches when heightInCentimeters changes
+            heightInInches = heightInCentimeters / 2.54
+        }
+    }
+    
+    // Initializer for height in inches
+    init(heightInInches: Double) {
+        self.heightInInches = heightInInches
+        self.heightInCentimeters = heightInInches * 2.54
+    }
+    
+    // Initializer for height in centimeters
     init(heightInCentimeters: Double) {
         self.heightInCentimeters = heightInCentimeters
-        self.heightInInches = heightInCentimeters/2.54
+        self.heightInInches = heightInCentimeters / 2.54
     }
 }
 
+var myHeight = Height(heightInInches: 65)
 
+print("Height in Centimeters: \(myHeight.heightInCentimeters)")
 
+myHeight.heightInInches = 70
+print("Updated Height in Centimeters: \(myHeight.heightInCentimeters)") 
 /*:
 [Previous](@previous)  |  page 7 of 10  |  [Next: App Exercise - Mile Times and Congratulations](@next)
  */
