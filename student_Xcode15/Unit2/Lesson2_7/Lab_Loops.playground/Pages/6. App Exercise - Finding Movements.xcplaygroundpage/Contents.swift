4eb5002a762463ba2d23229481334e785bebbcff
icazajimenezc7853 <142323737+icazajimenezc7853@users.noreply.github.com>
2024-11-24 14:12:17 -0500

    lab loops completed

--- student_Xcode15/Unit2/Lesson2_7/Lab_Loops.playground/Pages/6. App Exercise - Finding Movements.xcplaygroundpage/Contents.swift
+++ student_Xcode15/Unit2/Lesson2_7/Lab_Loops.playground/Pages/6. App Exercise - Finding Movements.xcplaygroundpage/Contents.swift
@@ -11,8 +11,12 @@
 let lowHR = 110
 let highHR = 125
 var movementHeartRates: [String: Int] = ["Walking": 85, "Running": 120, "Swimming": 130, "Cycling": 128, "Skiing": 114, "Climbing": 129]
-
-
+for (movement, heartRate) in movementHeartRates {
+    // Check if the heart rate is within the desired range
+    if heartRate >= lowHR && heartRate <= highHR {
+        print("You could go \(movement).")
+    }
+}
 /*:
  _Copyright © 2023 Apple Inc._
 
