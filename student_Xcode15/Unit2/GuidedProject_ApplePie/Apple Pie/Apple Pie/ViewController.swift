5f569b8f464cc9b2103302b4f4c0fbe5e06b8131
icazajimenezc7853 <142323737+icazajimenezc7853@users.noreply.github.com>
2024-12-01 19:40:12 -0500

    Merge branch 'main' of https://github.com/icazajimenezc7853/SwiftFundamentals15 into main

--- student_Xcode15/Unit2/GuidedProject_ApplePie/Apple Pie/Apple Pie/ViewController.swift
+++ student_Xcode15/Unit2/GuidedProject_ApplePie/Apple Pie/Apple Pie/ViewController.swift
@@ -1,39 +1,22 @@
-//
-//  ViewController.swift
-//  Apple Pie
-//
-//  Created by remotestudent on 12/1/24.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
     var listOfWords = ["buccaneer", "swift", "glorious", "incandescent", "bug", "program"]
     let incorrectMovesAllowed = 7
-    var totalWins = 0 {
-        didSet {
-            newRound()
-        }
-    }
-    var totalLosses = 0 {
-        didSet {
-            newRound()
-        }
-    }
+    var totalWins = 0
+    var totalLosses = 0
     var currentGame: Game!
     var pastGameWord: String = "" // hold the current game word
-    var currentWordIndex: Int = 0 // Track the current word index
 
     @IBOutlet var treeImageView: UIImageView!
     @IBOutlet var correctWordLabel: UILabel!
     @IBOutlet var scoreLabel: UILabel!
-        
+    
     @IBOutlet var letterButtons: [UIButton]!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        newRound()
+        newRound() // Start the first round
     }
         
     @IBAction func letterButtonPressed(_ sender: UIButton) {
@@ -47,10 +30,10 @@ class ViewController: UIViewController {
     func updateGameState() {
         if currentGame.incorrectMovesRemaining == 0 {
             totalLosses += 1
-            showRoundOver() // call the method to show the correct word
+            showRoundOver() // Show the round over message
         } else if currentGame.word == currentGame.formattedWord {
             totalWins += 1
-            showRoundOver() // call the method to show the correct word
+            showRoundOver() // Show the round over message
         } else {
             updateUI()
         }
@@ -58,22 +41,38 @@ class ViewController: UIViewController {
     
     func showRoundOver() {
         let alert = UIAlertController(title: "Round Over", message: "The correct word was: \(currentGame.word)", preferredStyle: .alert)
-        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: {_ in self.currentWordIndex += 1 //move to next word
-            self.newRound() //start next round
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { _ in
+            self.newRound() // Start the next round when the alert is dismissed
         }))
         present(alert, animated: true, completion: nil)
     }
     
     func newRound() {
-        if currentWordIndex < listOfWords.count {
-            pastGameWord = currentGame?.word ?? ""// save the current word
-            let newWord = listOfWords[currentWordIndex]
+        if !listOfWords.isEmpty {
+            pastGameWord = currentGame?.word ?? "" // Save the current word
+            let newWord = listOfWords.removeFirst()
             currentGame = Game(word: newWord, incorrectMovesRemaining: incorrectMovesAllowed, guessedLetters: [])
             enableLetterButtons(true)
-            updateUI()        
+            updateUI()
         } else {
-            enableLetterButtons(false)
+            // Game is finished, show final results
+            showFinalResults()
         }
+    }
+    
+    func showFinalResults() {
+        let alert = UIAlertController(title: "Game Over", message: "Total Wins: \(totalWins), Total Losses: \(totalLosses)", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { _ in
+            self.resetGame() // Reset the game if desired
+        }))
+        present(alert, animated: true, completion: nil)
+    }
+    
+    func resetGame() {
+        totalWins = 0
+        totalLosses = 0
+        listOfWords.shuffle() // Shuffle words for a new game
+        newRound() // Start a new game
     }
     
     func enableLetterButtons(_ enable: Bool) {
@@ -92,6 +91,4 @@ class ViewController: UIViewController {
         scoreLabel.text = "Wins: \(totalWins), Losses: \(totalLosses)"
         treeImageView.image = UIImage(named: "Tree \(currentGame.incorrectMovesRemaining)")
     }
-
 }
-
